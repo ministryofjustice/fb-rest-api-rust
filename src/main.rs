dd9996f399f9a98455eeceb5cd5ea0508031a96c
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-10-06 11:15:56 +0100

    Initial - trying to add post route

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+//pub mod schema;
+//pub mod models;
+
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
+
+//use crate::models::Service;
+use serde::{Serialize, Deserialize};
+use rocket_contrib::json::{Json, JsonValue};
 
 #[get("/health")]
 fn health() -> &'static str {
     "ok"
 }
+
+#[derive(Deserialize)]
+struct ServiceParam {
+    name: String
+}
+
+#[post("/services", format = "json", data = "<params>")]
+fn create_service(params: Json<ServiceParam>) -> JsonValue {
+    json!({ "status": "yay!" })
+}
+
+//#[get("/services/<id>")]
+//fn service(id: str) {
+//}
 
 #[database("db")]
 struct DbConn(diesel::PgConnection);
